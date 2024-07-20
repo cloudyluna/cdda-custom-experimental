8961100f098ba36b107e519690e68aed246a989d
cloudyluna <luna.cloudberry@proton.me>
2024-07-20 16:49:47 +0800

    Format flake.nix

--- flake.nix
+++ flake.nix
@@ -2,87 +2,80 @@
   description = "cdda-experimental-git";
 
   inputs = {
-    nixpkgs.url      = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url  = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
-  
+
   outputs = { self, nixpkgs, flake-utils, ... }:
     let supportedSystems = [ "x86_64-linux" ];
     in flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-        };
+      let pkgs = import nixpkgs { inherit system; };
       in {
         packages = rec {
           default = cdda-experimental-git;
 
           # With extra mods and all the goodies I like.
-          cdda-experimental-git-extras = pkgs.lib.overrideDerivation cdda-experimental-git (oldAttrs:
-            let mods = {
+          cdda-experimental-git-extras =
+            pkgs.lib.overrideDerivation cdda-experimental-git (oldAttrs:
+              let
+                mods = {
                   tank = builtins.fetchGit {
-                    url = "https://github.com/chaosvolt/cdda-tankmod-revived-mod";
+                    url =
+                      "https://github.com/chaosvolt/cdda-tankmod-revived-mod";
                     rev = "70278e9576a875c801ff6848e059312ae97a411c";
                   };
                 };
-            in {
-              installPhase = ''
-                runHook preInstall
+              in {
+                installPhase = ''
+                  runHook preInstall
 
-                mkdir $out
-                cp -R ${mods.tank}/Tankmod_Revived data/mods/
-                cp -R data gfx doc $out
-                
-                install -m755 -D cataclysm-tiles $out/bin/cataclysm-tiles
+                  mkdir $out
+                  cp -R ${mods.tank}/Tankmod_Revived data/mods/
+                  cp -R data gfx doc $out
 
-                cat << EOF > launcher
-                #!${pkgs.runtimeShell}
-                $out/bin/cataclysm-tiles --basepath $out --userdir \$HOME/.cdda-experimental-git
-                EOF
-                install -m755 -D launcher $out/bin/${oldAttrs.name}
+                  install -m755 -D cataclysm-tiles $out/bin/cataclysm-tiles
 
-                runHook postInstall
-              '';
-            });
-          
+                  cat << EOF > launcher
+                  #!${pkgs.runtimeShell}
+                  $out/bin/cataclysm-tiles --basepath $out --userdir \$HOME/.cdda-experimental-git
+                  EOF
+                  install -m755 -D launcher $out/bin/${oldAttrs.name}
+
+                  runHook postInstall
+                '';
+              });
+
           cdda-experimental-git = pkgs.stdenvNoCC.mkDerivation rec {
             name = "cdda-tiles-launcher";
             version = "2024-06-26-1623";
-            src =
-              pkgs.fetchurl {
-                url = "https://github.com/CleverRaven/Cataclysm-DDA/releases/download/cdda-experimental-${version}/cdda-linux-tiles-sounds-x64-${version}.tar.gz";
-                hash = "sha256-wQFRuJQcfBjRh1WY0/uUUAgIMMMsB/rq3H1UlDixhag=";
-              };
-            
-            nativeBuildInputs = with pkgs; [
-              autoPatchelfHook
-            ];
-            
-            buildInputs = with pkgs; [
-              SDL2
-              SDL2_image
-              SDL2_mixer
-              SDL2_ttf
-            ];
-            
+            src = pkgs.fetchurl {
+              url =
+                "https://github.com/CleverRaven/Cataclysm-DDA/releases/download/cdda-experimental-${version}/cdda-linux-tiles-sounds-x64-${version}.tar.gz";
+              hash = "sha256-wQFRuJQcfBjRh1WY0/uUUAgIMMMsB/rq3H1UlDixhag=";
+            };
+
+            nativeBuildInputs = with pkgs; [ autoPatchelfHook ];
+
+            buildInputs = with pkgs; [ SDL2 SDL2_image SDL2_mixer SDL2_ttf ];
+
             dontStrip = true;
 
             installPhase = ''
-                runHook preInstall
+              runHook preInstall
 
-                mkdir $out
-                cp -R data gfx doc $out
-                
-                install -m755 -D cataclysm-tiles $out/bin/cataclysm-tiles
+              mkdir $out
+              cp -R data gfx doc $out
 
-                cat << EOF > launcher
-                #!${pkgs.runtimeShell}
-                $out/bin/cataclysm-tiles --basepath $out --userdir \$HOME/.cdda-experimental-git
-                EOF
-                install -m755 -D launcher $out/bin/${name}
+              install -m755 -D cataclysm-tiles $out/bin/cataclysm-tiles
 
-                runHook postInstall
-              '';
+              cat << EOF > launcher
+              #!${pkgs.runtimeShell}
+              $out/bin/cataclysm-tiles --basepath $out --userdir \$HOME/.cdda-experimental-git
+              EOF
+              install -m755 -D launcher $out/bin/${name}
+
+              runHook postInstall
+            '';
           };
         };
       });
